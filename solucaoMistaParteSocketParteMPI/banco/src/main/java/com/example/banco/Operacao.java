f0d37bdae10a4182acb2f7da74d27d1cd61e86de
luca-moraes <lcansd.lm@gmail.com>
2023-12-10 01:18:03 -0300

    update java code

--- solucaoMistaParteSocketParteMPI/banco/src/main/java/com/example/banco/Operacao.java
+++ solucaoMistaParteSocketParteMPI/banco/src/main/java/com/example/banco/Operacao.java
@@ -3,3 +3,15 @@ public interface Operacao {
     void aplicar();
     void reverter();
 }
+
+// class Cliente implements Operacao{
+// @Override
+// public synchronized void aplicar() {
+//     // Lógica para aplicar a operação no saldo do cliente
+//  }
+
+// @Override
+// public synchronized void reverter() {
+//     // Lógica para reverter a operação no saldo do cliente
+//  }
+// }
